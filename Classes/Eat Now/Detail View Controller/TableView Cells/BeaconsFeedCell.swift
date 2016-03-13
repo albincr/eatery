5da5050924ead226810f333428c33663322072ea
myo3 <myo3@users.noreply.github.com>
2016-03-13 15:42:58 -0400

    Set up nib file for beacons feed cell

---
+++ Classes/Eat Now/Detail View Controller/TableView Cells/BeaconsFeedCell.swift
@@ -0,0 +1,13 @@
+//
+//  BeaconsFeedCell.swift
+//  Eatery
+//
+//  Created by Monica Ong on 3/13/16.
+//  Copyright © 2016 CUAppDev. All rights reserved.
+//
+
+import UIKit
+
+class BeaconsFeedCell: UICollectionViewCell {
+    
+}
